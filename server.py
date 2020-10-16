d2b20267ed69900ba3b4fdb8172c69f6fc4618df
sunnyxdaz5 <sunnyxdaz07@gmail.comclear>
2020-10-16 23:55:35 +0600

    my portfolio website

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        file = database.write(f'\n{name},{email},{message}')
+
+
+def write_to_csv(data):
+    with open('database.csv',newline='', mode='a') as database2:
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name, email, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return 'Thank you for contacting...I\'ll get in touch with you shortly'
+    else:
+        return 'something went wrong. Try again!'
